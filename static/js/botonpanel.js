4fbf7463a1c39696e8ee685b99c55bfba8ffd067
biancaRomero <bianca.romero@unmsm.edu.pe>
2022-12-13 12:19:00 -0500

    .

--- static/js/botonpanel.js
+++
@@ -1,8 +0,0 @@
-/*Aqui se definen las funciones generales que ejecutaran los botones
-dentro de los paneles*/
-function cargarPanel(panelID){
-    limpiarPantalla()
-    const nuevoPanel = document.getElementById(panelID)
-    nuevoPanel.style.display="block"
-}
-
